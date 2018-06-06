b5bae8456b01b8e155df44c8dc64a83c54716e47
Anastasia <panasovanastya@gmail.com>
2018-06-06 15:46:39 +0300

    решение  задачи

---
+++ task2.js
@@ -0,0 +1,24 @@
+var d = parseInt(process.argv[2]);
+var m = process.argv[3];
+var y = parseInt(process.argv[4]);
+
+function leapYear(year) { 
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0); 
+}
+
+function sumDays(arr, n){
+    var sum = 0;
+    for(var i = 0; i < n; i++){
+        sum += parseInt(arr[i]);
+    }
+    return sum;
+}
+
+var k = leapYear(y)?1:0;
+var days = [31, 28 + k, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+var mx = ['января', 'февраля', 'марта', 'апреля', 
+          'мая', 'июня', 'июля', 'августа', 
+          'сентября', 'октября', 'ноября', 'декабря'];
+
+var result = (365 + k) - sumDays(days, mx.indexOf(m)) - d;
+process.stdout.write(result.toString());
