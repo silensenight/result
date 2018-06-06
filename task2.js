f684397fcac7b100602407fadb6bfe5fc19e86c2
Anastasiya <31617112+silensenight@users.noreply.github.com>
2018-06-06 15:50:27 +0300

    Update task2.js
    решение 2 задачи

--- task2.js
+++ task2.js
@@ -2,23 +2,45 @@ var d = parseInt(process.argv[2]);
 var m = process.argv[3];
 var y = parseInt(process.argv[4]);
 
-function leapYear(year) { 
-    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0); 
+const months = {
+  января: 0,
+  февраля: 1,
+  марта: 2,
+  апреля: 3,
+  мая: 4,
+  июня: 5,
+  июля: 6,
+  августа: 7,
+  сентября: 8,
+  октября: 9,
+  ноября: 10,
+  декабря: 11,
+};
+
+const days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+function addDay(year) {
+  if (year % 400 === 0) {
+    return true;
+  }
+  if (year % 4 === 0 && year % 100 !== 0) {
+    return true;
+  }
+  return false;
 }
 
-function sumDays(arr, n){
-    var sum = 0;
-    for(var i = 0; i < n; i++){
-        sum += parseInt(arr[i]);
-    }
-    return sum;
+function countDays(d, m, y) {
+  let num = days[months[m]] - d > 0 ? days[months[m]] - d : 0;
+
+  for (let i = months[m] + 1; i < 12; i++) {
+    num += days[i];
+  }
+
+  if (months[m] === 1 || (months[m] === 2 && d <= 28)) {
+    num += addDay(y);
+  }
+
+  return num;
 }
 
-var k = leapYear(y)?1:0;
-var days = [31, 28 + k, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-var mx = ['января', 'февраля', 'марта', 'апреля', 
-          'мая', 'июня', 'июля', 'августа', 
-          'сентября', 'октября', 'ноября', 'декабря'];
-
-var result = (365 + k) - sumDays(days, mx.indexOf(m)) - d;
-process.stdout.write(result.toString());
+process.stdout.write(countDays(d, m, y));
